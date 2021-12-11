4f41c9a5de04fcd0774a0b9f53c72a32f585a60c
Max Kämmerer <maxkaemmerer.hb@gmail.com>
2021-12-11 21:34:03 +0100

    - add type for two parameter function

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,14 @@
-type OneParamFunction<A, B> = (value: A) => B
+type OneParamFunction<T, T2> = (value: T) => T2
+type TwoParamFunction<T, T2, T3> = (a: T, b: T2) => T3
 
-export function compose<A, B, C>(f1: OneParamFunction<B, C>, f2: OneParamFunction<A, B>): OneParamFunction<A, C> {
-    return (a: A) => f1(f2(a));
+export function compose<T, T2, T3>(f1: OneParamFunction<T2, T3>, f2: OneParamFunction<T, T2>): OneParamFunction<T, T3> {
+    return (a: T) => f1(f2(a));
 }
 
-export function curry2<T,T2,T3>(f: (a: T, b: T2) => T3): (a: T) => (b: T2) => T3{
+export function curry2<T, T2, T3>(f: TwoParamFunction<T, T2, T3>): (a: T) => (b: T2) => T3 {
     return (a: T) => (b: T2) => f(a, b);
 }
 
-export function flip<T,T2,T3>(f: (a: T, b: T2) => T3): (b: T2, a: T) => T3 {
+export function flip<T, T2, T3>(f: TwoParamFunction<T, T2, T3>): (b: T2, a: T) => T3 {
     return (b: T2, a: T) => f(a, b);
 }
